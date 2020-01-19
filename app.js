f09cc9ae287ce9de94bc277590d326c80f35f09a
Luma <luma@icarustech.com.br>
2020-01-19 14:48:20 -0300

    Validating if move is in algebric notation.

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(cors());
+app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-  res.send('Got a POST request');
+app.get('/possible-moves', () => {
+
+});
+
+app.post('/move', (req, res) => {
+
+  const cellName = req.body.cellName;
+
+  if (cellName === undefined) {
+    res.status(500).json({ error: 'Please pass the cell name on the request' })
+  }
+
+  const x = cellName.substr(0, 1);
+  const y = cellName.substr(1, 2);
+
+  const regexLetters = "[ABCDEFGH]+";
+  const regexNumbers = "[12345678]+";
+
+  if (regexLetters === null || regexNumbers === null) {
+    res.status(500).json({ error: 'Please use algebric notation!' })
+  }
 })
 
 app.listen(3001, () => console.log('App listening on port 3001!'));
