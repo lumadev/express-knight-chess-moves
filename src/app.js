269dbcb733ba1269d6bba28c3110728ab4908df0
Luma <luma@icarustech.com.br>
2020-01-21 00:36:03 -0300

    Exported functions to allow testing.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(require('./routes'));
+app.use(routes);
 
 app.listen(3001, () => console.log('App listening on port 3001!'));
